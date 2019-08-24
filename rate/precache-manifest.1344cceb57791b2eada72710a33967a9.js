23c7a2a95051c44cc6ef37c6d02e3824716c18ef
bizir <bizir3@gmail.com>
2019-08-24 20:23:04 +0300

    0.096

---
+++ rate/precache-manifest.1344cceb57791b2eada72710a33967a9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "d1fd903c38bf34cf1a3a",
+    "url": "/rate/static/css/main.b142ab92.chunk.css"
+  },
+  {
+    "revision": "d1fd903c38bf34cf1a3a",
+    "url": "/rate/static/js/main.29bdcff9.chunk.js"
+  },
+  {
+    "revision": "96898c2f9186524f1b66",
+    "url": "/rate/static/js/runtime~main.2952b077.js"
+  },
+  {
+    "revision": "181043a4e54fa4bf7d7d",
+    "url": "/rate/static/css/2.4e9e6b85.chunk.css"
+  },
+  {
+    "revision": "181043a4e54fa4bf7d7d",
+    "url": "/rate/static/js/2.1359dd22.chunk.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/rate/static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "515d2fa951ef2df0abbce7f0be4fc18c",
+    "url": "/rate/static/media/Table.515d2fa9.less"
+  },
+  {
+    "revision": "c668520f2ebd1551b469399e196e5260",
+    "url": "/rate/index.html"
+  }
+];
--- rate/service-worker.js
+++ rate/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/rate/precache-manifest.ce7f4e498e7768cf246e689efeeacd00.js"
+  "/rate/precache-manifest.1344cceb57791b2eada72710a33967a9.js"
 );
 
 workbox.clientsClaim();
