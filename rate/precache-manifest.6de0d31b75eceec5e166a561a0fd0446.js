8aae5cc0f30d33d83646d7867afe2f4931498387
bizir <bizir3@gmail.com>
2019-08-27 21:08:55 +0300

    0.098

---
+++ rate/precache-manifest.6de0d31b75eceec5e166a561a0fd0446.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9c526af802116d805f58",
+    "url": "/rate/static/js/main.54f7e077.chunk.js"
+  },
+  {
+    "revision": "96898c2f9186524f1b66",
+    "url": "/rate/static/js/runtime~main.2952b077.js"
+  },
+  {
+    "revision": "823e25da3a22bff1bb62",
+    "url": "/rate/static/js/2.3ea890eb.chunk.js"
+  },
+  {
+    "revision": "4652b705f904e1b2e49361584adb1216",
+    "url": "/rate/static/media/WebPage.4652b705.less"
+  },
+  {
+    "revision": "4e88bcd191434927efcb2298261a7cda",
+    "url": "/rate/static/media/index.4e88bcd1.less"
+  },
+  {
+    "revision": "18f5f23be568f5938dc4849c6b8b4a8a",
+    "url": "/rate/index.html"
+  }
+];
