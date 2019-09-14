458b1afbbbcf8ced28d6ffd3089ae84a6047ab42
bizir <bizir3@gmail.com>
2019-09-14 18:37:13 +0300

    0.01

---
+++ access/precache-manifest.2b5a398c685efd448d5fce792ef62b69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "337787a6b0f705d50093",
+    "url": "/access/static/css/main.ea7295c1.chunk.css"
+  },
+  {
+    "revision": "337787a6b0f705d50093",
+    "url": "/access/static/js/main.752c0566.chunk.js"
+  },
+  {
+    "revision": "78d81e5e1f84f4f01900",
+    "url": "/access/static/js/runtime~main.ea4a348e.js"
+  },
+  {
+    "revision": "d3674b3bc82e433acd9f",
+    "url": "/access/static/css/2.e370f37d.chunk.css"
+  },
+  {
+    "revision": "d3674b3bc82e433acd9f",
+    "url": "/access/static/js/2.450f10ef.chunk.js"
+  },
+  {
+    "revision": "3a91c50225151a1b636615ea40d22952",
+    "url": "/access/index.html"
+  }
+];
